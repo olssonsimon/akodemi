a7f4da9e7438ecc3e348dee0ad5df9d9a171225e
simon olsson <olssonsim@gmail.com>
2014-01-24 12:01:56 +0100

    added secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true
 	validates :email, uniqueness: true
+	validates :password, length: { minimum: 6 }
 
 	validates :name, length: { maximum: 50 }
 
 	# email format validation
 	validates :email, email: true
+
+	has_secure_password
 end
