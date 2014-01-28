0740b9740002a6a4901243c4c7f02daefdc7a36f
simon olsson <olssonsim@gmail.com>
2014-01-28 16:08:24 +0100

    sign in out is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:show, :edit, :destroy]
+	before_action :correct_user, only: [:show, :edit, :destroy]
 
 	def index
 		redirect_to root_url
@@ -12,10 +14,12 @@ class UsersController < ApplicationController
 		@user = !params[:id].nil? ? User.find(params[:id]) : current_user
 	end
 
+	def edit
+	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			# Handle signup success
 			sign_in @user
 			flash[:success] = "Welcome to Akodemi!"
 			redirect_to @user
@@ -26,7 +30,7 @@ class UsersController < ApplicationController
 
 	def destroy
 		User.find(params[:id]).destroy
-		flash[:success] = "User deleted"
+		flash[:success] = "Din profil har tagits bort."
 		redirect_to users_url
 	end
 
@@ -34,5 +38,16 @@ class UsersController < ApplicationController
 
 		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar, :bio)
+		end
+
+		# Before filters
+
+		def signed_in_user
+			redirect_to signin_url, notice: "Du måste logga in för att besöka den sidan." unless signed_in?
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to root_url, notice: "Du har inte åtkomst till den här sidan." unless current_user?(@user)
 		end
 end
